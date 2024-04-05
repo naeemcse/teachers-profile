07c26350a68b84f28d4f56c1c65826a635e1c138
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-04-05 07:07:43 +0600

    Hero section's photo height is changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Paragraph from '@/components/Paragraph';
-
 const Hero = () => {
     const paragraphText = `SHITO-RYU KARATE
     non, amet, nibh ullamcorper ex. laoreet amet, commodo fringilla leo. at, scelerisque tincidunt enim. facilisis amet, urna. odio odio non urna. nibh id eu nisi . Morbi odio nisl. nulla, vel dui. nisi sit orci quis efficitur. tincidunt quis nisl. sed Sed ex est. varius Nam quis eget lorem. id Donec libero, est. eget non .
@@ -12,10 +11,10 @@ const Hero = () => {
             <div className="md:ml-10">
                 <h1 className='uppercase text-primary font-extrabold font-2xl text-base'> Karate Course </h1>
                 <div className="md:flex justify-center">
-                   <div className="max-w-[303] h[200px] md:ml-5">
+                   <div className="max-w-1/2 h[200px] md:ml-5">
                           <img className="rounded-md" src="/profile.avif" alt="karate" />
                    </div>
-                   <div className="max-w-[600px] h[260px] px-4">
+                   <div className="max-w-1/2 h[260px] px-4">
                     <h1 className='uppercase text-primary font-extrabold '> Sito-ryu karate  </h1>
                     <div className=''>
                      <Paragraph text={paragraphText} maxLength={maxLength} />
